999bea361883824bb23fbb0429192c097484796c
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-03-27 18:54:51 -0600

    ciclo for en shell

--- for.sh
+++ for.sh
@@ -1,4 +1,14 @@
-for f in a b c
+#!/bin/bash
+
+for f in {a..z}
 do 
 	echo "la variable es: " $f
 done
+
+
+
+#para revisar cuantos ficheros hay en la ruta que me encuentro
+#for f in *
+#do 
+#	echo "el fichero es: " $f
+#done
