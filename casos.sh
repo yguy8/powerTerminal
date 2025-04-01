1ece888efb5a1cd897db5a89cb7c948da14a832c
Whyaba <clara20franco@gmail.com>
2025-04-01 16:58:31 -0600

    Create casos.sh
    un nuevo script en bash

---
+++ casos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+case ${1,,} in 
+	clara | administrador)
+		echo "Hola de nuevo"
+		;;
+	help)
+		echo "Ingresa un nombre"
+		;;
+	*)
+		echo "Hola, no se quien eres"
+esac 
+//es para ver quien es el administador y saludarlo según lo que conteste
