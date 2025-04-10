8cf4df0b58501e7d75b4b38d661cf1b910d11cc5
Whyaba <clara20franco@gmail.com>
2025-04-10 00:20:37 -0600

    Crear archivos

--- creararch.sh
+++ creararch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo "Este programa es para crear archivos de texto"
+echo "Este programa es para crear archivos de texto (no es necesario añadir una extensión ya que la tiene por defecto y es txt de texto)"
 echo "Ponle un nombre a tu archivo"
 read nombre
 nwfl=$(touch $nombre.txt)
