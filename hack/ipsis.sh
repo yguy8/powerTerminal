7eb3541b71e3c52835b1ff0019ea40abf59cedbd
Whyaba <clara20franco@gmail.com>
2025-04-10 00:13:01 -0600

    script para saber que maquina es si un linux o un windows

--- hack/ipsis.sh
+++ hack/ipsis.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 host=$1
+
+echo "Vamos a analizar la siguiete dirección IP: $host"
+
+ping -c 1 $host > ping.log
+
+for i in $(seq 60 70);do
+        if test $(grep ttl=$i  ping.log -c) = 1; then
+                echo "Es un LINUX"
+        fi
+done
+
+for i in $(seq 100 200); do
+        if test $(grep ttl=$i ping.log -c) = 1; then
+                echo "Es un WINDOWS"
+        fi
+done
+
+rm ping.log
+
