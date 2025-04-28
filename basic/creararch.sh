6ea59bdc8c365d39dcaf0e628a3a77cced1c5a8f
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-04-28 10:13:48 -0600

    Reoganización de archivos

--- basic/creararch.sh
+++ basic/creararch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 echo "Este programa es para crear archivos de texto (no es necesario añadir una extensión ya que la tiene por defecto y es txt de texto)"
-echo "Ponle un nombre a tu archivo"
+echo "Ponle un nombre a tu archivo" 
 read nombre
 nwfl=$(touch $nombre.txt)
 
