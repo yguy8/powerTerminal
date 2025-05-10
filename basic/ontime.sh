cfc112a80e6279638e5081952e9ff4b5b2c9c8d3
Clara <clara20franco@gmail.com>
2025-05-09 18:57:00 -0600

    what is my computer on time?

--- basic/ontime.sh
+++ basic/ontime.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-showname(){
-        echo Holanda $1
-	if [ ${1,,} = root ]; then
-		return 0
-	else
-		return 1
-	fi
-}
-showname $1
-if [ $? = 1 ]; then 
-	echo "No te conozco"
-fi
-
 #!/bin/bash 
 
 showuptime(){
